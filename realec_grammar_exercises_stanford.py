9f3a195a2390c7e0d8f0ec3bb19d69aaf190c8de
nicklogin <niklogin10v@yandex.ru>
2017-11-05 16:22:10 +0300

    Update realec_grammar_exercises_stanford.py

--- realec_grammar_exercises_stanford.py
+++ realec_grammar_exercises_stanford.py
@@ -469,7 +469,7 @@ class Exercise:
         #смотрим на то, какой массив мы передали объекту, так как отследить, какая ошибка обрабатывается сейчас мы уже не можем
         if self.error_type == ['Number']:
             quantifiers = ('some', 'someone', 'somebody', 'one', 'everyone', 'everybody', 'noone', 'no-one', 'nobody', 'something', 'everything', 'nothing')
-            if vff.find_verb_forms(wrong.split()[0]):
+            if nltk.tag.staford(right.split()[0])[1].startswith('V') and nltk.tag.stanford([wrong.split()[0]])[1].startswith('V'):
                 quant_presence = False
                 tagged_sent = self.tagger.tag(new_sent.replace('_______',right).split())
                 for i in range(len(tagged_sent)):
